bca3005805f37d0cb831f47b8a036bb621751d72
shinyscyther <mesibovj@gmail.com>
2019-03-02 16:22:54 -0500

    created the temporary featured.json file to use until we set up the mongo DB, worked in the FeaturedComponent folder to set up the Card.js file so we can render the featured items as cards in the featured page. Still working on applying the necessary bootstrap to the cards.

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,17 +1,41 @@
 import React from "react";
-import Admintestcomponent from "./AdminComponent/Admintestcomponent";
+import AdminTest from "./AdminComponent/Admintestcomponent";
+import featured from "./AdminComponent/featured.json";
 
 
 class Admin extends React.Component {
+  state = {
+    featuredList: featured
+  };
   render() {
+    const { featuredList } = this.state;
+    console.log(featured);
+
     return (
-      <div>Admin
-
-        <Admintestcomponent />
-
+      // <div>Admin</div>
+      <div>
+        <h1 className="title">Featured</h1>
+        {featuredList.map(featured => {
+          return (
+            <AdminTest
+              key={featured.id}
+              featuredId={featured.id}
+              name={featured.name}
+              image={featured.image}
+              type={featured.type}
+              price={featured.price}
+            />
+          );
+        })}
       </div>
+
     );
   }
 }
 
 export default Admin;
+
+
+
+
+
---
+++ src/components/FeaturedComponent/Card.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+
+function FeaturedCard(props) {
+
+  return (
+    <div className="container-fluid mx-auto card-content">
+      <h3 className="text-center">Featured Deals</h3>
+      <div className="row container-fluid">
+        <div className="col-lg-3 col-md-4 col-sm-6 my-2">
+          <div className="card mx-auto">
+            <div>
+              <img className="card-img-top img-fluid" src={props.image} alt={props.name} />
+            </div>
+            <div className="card-body">
+              <h5 className="card-title text-center">{props.name}</h5>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+
+export default FeaturedCard;
