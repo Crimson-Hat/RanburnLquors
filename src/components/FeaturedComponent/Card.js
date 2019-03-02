154da4b1652d8bc08be464e2c48e8fbf4e6a48c3
shinyscyther <mesibovj@gmail.com>
2019-03-02 17:14:50 -0500

    Got the necessardded the bootstrap link in the index.html file. Still need to pretty up the cards

--- src/components/FeaturedComponent/Card.js
+++ src/components/FeaturedComponent/Card.js
@@ -1,11 +1,10 @@
 import React from "react";
-
+import "./style.css";
 
 function FeaturedCard(props) {
 
   return (
     <div className="container-fluid mx-auto card-content">
-      <h3 className="text-center">Featured Deals</h3>
       <div className="row container-fluid">
         <div className="col-lg-3 col-md-4 col-sm-6 my-2">
           <div className="card mx-auto">
@@ -14,6 +13,17 @@ function FeaturedCard(props) {
             </div>
             <div className="card-body">
               <h5 className="card-title text-center">{props.name}</h5>
+              <div className="row">
+                <div className="col-6">
+                  {props.size}
+                
+                  {props.alc}
+                </div>
+                <div className="col-6">
+                  <h4 className="card-title text-right font-weight-light text-danger"><del>{props.was}</del></h4>
+                  <h3 className="card-title text-right">{props.price}</h3>
+                </div>
+              </div>
             </div>
           </div>
         </div>
