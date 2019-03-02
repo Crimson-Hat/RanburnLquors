44fc5de8915d5e3501ad1a6fdc73216f41dd1b87
Priyesh <91priyesh@gmail.com>
2019-03-02 13:28:56 -0500

    test successfull of loading in component from other files

---
+++ src/components/AdminComponent/Admintestcomponent.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+class Admintestcomponent extends Component {
+  render() {
+    return (
+      <div>Admintestcomponent</div>
+    )
+  }
+}
+
+export default Admintestcomponent;
