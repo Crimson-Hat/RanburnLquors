7bc459854d30c97092d322365aa6406a8d73849e
shinyscyther <mesibovj@gmail.com>
2019-03-02 19:00:35 -0500

    made progress with the responsiveness of the cards

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -14,22 +14,30 @@ class Featured extends React.Component {
     return (
       <div>
         <h1 className="title">Featured</h1>
-        {featuredList.map(featured => {
-          return (
-          <FeaturedCard
-          key = { featured.id }
-          featuredId = { featured.id }
-          name = { featured.name }
-          image = { featured.image }
-          type = { featured.type }
-          price = { featured.price }
-          size = { featured.size }
-          alc = { featured.alc }
-          was = { featured.was }
-          />
-          );
-        })}
-      </div>
+
+        <div className="container-fluid mx-auto card-content">
+          <div className="row container-fluid">
+            
+
+              {featuredList.map(featured => {
+                return (
+                  <FeaturedCard
+                    key={featured.id}
+                    featuredId={featured.id}
+                    name={featured.name}
+                    image={featured.image}
+                    type={featured.type}
+                    price={featured.price}
+                    size={featured.size}
+                    alc={featured.alc}
+                    was={featured.was}
+                  />
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      
     )
   }
 }
--- src/components/FeaturedComponent/Card.js
+++ src/components/FeaturedComponent/Card.js
@@ -4,31 +4,34 @@ import "./style.css";
 function FeaturedCard(props) {
 
   return (
-    <div className="container-fluid mx-auto card-content">
-      <div className="row container-fluid">
-        <div className="col-lg-3 col-md-4 col-sm-6 my-2">
-          <div className="card mx-auto">
-            <div>
-              <img className="card-img-top img-fluid" src={props.image} alt={props.name} />
+    <div className="col-lg-3 col-md-4 col-sm-6 my-2">
+      <div className="card mx-auto">
+        <div>
+          <img className="card-img-top img-fluid" src={props.image} alt={props.name} />
+        </div>
+        <div className="card-body">
+          <h4 className="card-title text-center">{props.name}
+          </h4>
+          <div className="row">
+            <div className="col-6 card-font-property pr-0">
+              {props.size}
+              <br />
+
+              {props.alc}<span className="card-font-property2">alc./vol.</span>
             </div>
-            <div className="card-body">
-              <h5 className="card-title text-center">{props.name}</h5>
-              <div className="row">
-                <div className="col-6">
-                  {props.size}
-                
-                  {props.alc}
-                </div>
-                <div className="col-6">
-                  <h4 className="card-title text-right font-weight-light text-danger"><del>{props.was}</del></h4>
-                  <h3 className="card-title text-right">{props.price}</h3>
-                </div>
-              </div>
+            <div className="col-6 card-font-property">
+              <div className="text-right font-weight-light text-danger"><del>{props.was}</del></div>
+          <div className="row text-right">
+            <div>
+              <div className="card-font-property3 text-right">{props.price}</div>
+            </div>
+          </div>
             </div>
           </div>
         </div>
       </div>
     </div>
+
   )
 }
 
