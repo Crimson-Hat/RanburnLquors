8b37379540f170932f90c470bade02b7583da42e
shinyscyther <mesibovj@gmail.com>
2019-03-05 18:47:49 -0500

    saving progress. Working on form for admin page

--- src/components/AdminComponent/Admintestcomponent.js
+++ src/components/AdminComponent/Admintestcomponent.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 
+
 // class Admintestcomponent extends Component {
 //   render() {
 //     return (
@@ -11,15 +12,36 @@ import React from 'react';
 
 function AdminTest(props) {
   return (
-    <div className="col-lg-3 col-md-4 col-sm-6 my-2">
-      <div className="card mx-auto">
-        <div>
-          <img className="card-img-top img-fluid" src={props.image} alt={props.name} />
+    <div className="container-fluid">
+      <form>
+
+        <div className="form-group">
+          <label for="itemUpdate">What item would you like to update?</label>
+          <select className="form-control" id="itemUpdate">
+            <option>Collier Creek Cabernet Sauvignon</option>
+            <option>Collier Creek Pinot Noir</option>
+            <option>Collier Creek Chardonnay</option>
+            <option>J.Moreau & Fils Vouvray</option>
+            <option>Jack Daniels</option>
+            <option>Bulleit Bourbon</option>
+            <option>Tito's Vodka</option>
+            <option>The Macellan 12yr Double Cask</option>
+          </select>
         </div>
-        <div className="card-body">
-          <h5 className="card-title text-center">{props.name}</h5>
+
+        <div className="form-group">
+        <label for="typeOfUpdate">How would you like to update this item?</label>
+        <select className="form-control" id="typeOfUpdate">
+          <option>Update item name</option>
+          <option>Update item image</option>
+          <option>Update item type</option>
+          <option>Update item price</option>
+          <option>Update item size</option>
+          <option>Update item's alcohol/volume concentration</option>
+          <option>Update item's old price</option>
+        </select>
         </div>
-      </div>
+      </form>
     </div>
   )
 }
