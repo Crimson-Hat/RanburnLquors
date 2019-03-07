932f5f93dd77d6d6cc3f3703354c217bb4f24224
Priyesh <91priyesh@gmail.com>
2019-03-07 14:39:43 -0500

    Saving changes to work on after going home

--- client/src/components/UniversalComponent/Navbar.js
+++ client/src/components/UniversalComponent/Navbar.js
@@ -6,6 +6,7 @@ function NavBar() {
 
   return (
     <nav>
+      {/* Logo */}
       <div className="row container-fluid">
         <div className="col-3"></div>
         <div className="col-sm">
@@ -15,39 +16,40 @@ function NavBar() {
         </div>
         <div className="col-3"></div>
       </div>
-      <div className="row container">
-
+      {/* NavLinks Line */}
+      <div className="row container-fluid">
         <div className="container col-3 text-center">
-          <NavLink
-            to='/wines'
-            className="tags"
-            activeStyle={{ color: 'red', fontWeight: 'bold'}}>
-            Wines
+        </div>
+      <div className="row container col-6">
+          <div className="col-4 text-center">
+            <NavLink
+              to='/wines'
+              className="tags"
+              activeStyle={{ color: 'red', fontWeight: 'bold' }}>
+              Wines
           </NavLink>
-        </div>
-        <div className="container col-3 text-center">
-          <NavLink
-            to='/spirits'
-            className="tags"
-            activeStyle={{ color: 'red', fontWeight: 'bold'}}>
-            Spirits
+          </div>
+          <div className="col-4 text-center">
+            <NavLink
+              to='/spirits'
+              className="tags"
+              activeStyle={{ color: 'red', fontWeight: 'bold' }}>
+              Spirits
           </NavLink>
+          </div>
+          <div className="col-4 text-center">
+            <NavLink
+              to='/beers'
+              className="tags"
+              activeStyle={{ color: 'red', fontWeight: 'bold' }}>
+              Beers
+            </NavLink>
+          </div>          
         </div>
         <div className="container col-3 text-center">
-        <NavLink 
-            to='/beers'
-            className="tags"
-            activeStyle={{ color: 'red', fontWeight: 'bold'}}>
-            Beers
-        </NavLink>
         </div>
-        <div className="container col-3 text-center">
-        </div>
-
-
-
       </div>
-    </nav>
+    </nav >
   )
 }
 
