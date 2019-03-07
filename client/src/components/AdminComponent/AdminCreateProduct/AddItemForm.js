cdf8f9532d05227f5cd3138666fb2e00969c1615
shinyscyther <mesibovj@gmail.com>
2019-03-06 23:50:11 -0500

    saving AddItem form

---
+++ client/src/components/AdminComponent/AdminCreateProduct/AddItemForm.js
@@ -0,0 +1,71 @@
+import React from 'react';
+
+
+
+function AddItem() {
+  return (
+    // <React.Fragment>
+    <div className="container-fluid">
+
+      <div className="card">
+
+      <h2>Add a Product</h2>
+
+        <form>
+
+          <div className="form-group">
+            <label for="newName">What is the Product's Name?</label>
+            <input type="text" className="form-control" id="newName" placeholder="Drink Name Here">
+          </div>
+
+          <div className="form-group">
+            <label for="newVolume">What is the Product's Volume?</label>
+            <input type="text" className="form-control" id="newVolume" placeholder="Drink Volume Here">
+          </div>
+
+          <div className="form-group">
+            <label for="newAbv">What is the Product's Abv?</label>
+            <input type="text" className="form-control" id="newAbv" placeholder="Drink Abv Here">
+          </div>
+
+          <div className="form-group">
+            <label for="newCategory">What is the Product's Category?</label>
+            <input type="text" className="form-control" id="newCategory" placeholder="Drink Category Here">
+          </div>
+
+          <div className="form-group">
+            <label for="newType">What is the Product's Type?</label>
+            <input type="text" className="form-control" id="newType" placeholder="Drink Type Here">
+          </div>
+
+          <div className="form-group">
+            <label for="newSubType">What is the Product's Sub-Type?</label>
+            <input type="text" className="form-control" id="newSubType" placeholder="Drink Sub-Type Here">
+          </div>
+
+          <div className="form-group">
+            <label for="newCountry">What is the Product's Country?</label>
+            <input type="text" className="form-control" id="newCountry" placeholder="Drink Country Here">
+          </div>
+
+          <div className="form-group">
+            <label for="newPrice">What is the Product's Price?</label>
+            <input type="text" className="form-control" id="newPrice" placeholder="Drink Name Here">
+          </div>
+
+          <div className="form-group">
+            <label for="newDescription">What is the Product's Description?</label>
+            <textarea className="form-control" id="newDescription" rows="4"></textarea>
+          </div>
+
+        </form>
+        
+      </div>
+
+      </div>
+      {/* </React.Fragment> */}
+      
+      )
+    }
+        
+export default AddItem;
--- client/src/components/AdminComponent/Admintestcomponent.js
+++ client/src/components/AdminComponent/Admintestcomponent.js
@@ -2,41 +2,48 @@ import React from 'react';
 
 
 
-function AdminTest(props) {
+function AdminTest() {
   return (
     <div className="container-fluid">
 
       <div className="card">
-      
-      <form>
 
-        <div className="form-group">
-          <label for="itemUpdate">What item would you like to update?</label>
-          <select className="form-control" id="itemUpdate">
-            <option>Collier Creek Cabernet Sauvignon</option>
-            <option>Collier Creek Pinot Noir</option>
-            <option>Collier Creek Chardonnay</option>
-            <option>J.Moreau & Fils Vouvray</option>
-            <option>Jack Daniels</option>
-            <option>Bulleit Bourbon</option>
-            <option>Tito's Vodka</option>
-            <option>The Macellan 12yr Double Cask</option>
-          </select>
-        </div>
+        <form>
 
-        <div className="form-group">
-        <label for="typeOfUpdate">How would you like to update this item?</label>
-        <select className="form-control" id="typeOfUpdate">
-          <option>Update item name</option>
-          <option>Update item image</option>
-          <option>Update item type</option>
-          <option>Update item price</option>
-          <option>Update item size</option>
-          <option>Update item's alcohol/volume concentration</option>
-          <option>Update item's old price</option>
-        </select>
-        </div>
-      </form>
+          <div className="form-group">
+            <label for="itemUpdate">What item would you like to update?</label>
+            <select className="form-control" id="itemUpdate">
+              <option>Rampur</option>
+              <option>Clontarf</option>
+              <option>Amrut Fusion</option>
+              <option>Hine Rare VSOP</option>
+              <option>Mejor Tequila Pink</option>
+              <option>Tito's Handmade Vodka</option>
+            </select>
+          </div>
+
+          <div className="form-group">
+            <label for="typeOfUpdate">How would you like to update this item?</label>
+            <select className="form-control" id="typeOfUpdate">
+              <option>Update Product Name</option>
+              <option>Update Volume (mL)</option>
+              <option>Update Abv (%alc./vol.)</option>
+              <option>Update Category</option>
+              <option>Update Type</option>
+              <option>Update Sub-Type</option>
+              <option>Update Country</option>
+              <option>Update Price</option>
+              <option>Update Description</option>
+              <option>DELETE PRODUCT</option>
+            </select>
+          </div>
+
+          <div className="form-group">
+            <label for="admin-input">What would you like to set the new value to, if applicable?</label>
+            <textarea className="form-control" id="adminInput" rows="3"></textarea>
+          </div>
+
+        </form>
       </div>
 
     </div>
