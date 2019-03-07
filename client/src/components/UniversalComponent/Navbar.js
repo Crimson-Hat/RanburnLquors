26e3c5879f3be08c735c09b33296b622f467b647
Priyesh <91priyesh@gmail.com>
2019-03-07 14:04:55 -0500

    added new route for jordan in middle of working with nav

--- client/src/components/UniversalComponent/Navbar.js
+++ client/src/components/UniversalComponent/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import logo from "./logo.png";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 function NavBar() {
 
@@ -9,11 +9,43 @@ function NavBar() {
       <div className="row container-fluid">
         <div className="col-3"></div>
         <div className="col-sm">
-          <Link  to="/">
-          <img className="img-fluid" src={logo} alt="logo" />
+          <Link to="/">
+            <img className="img-fluid" src={logo} alt="logo" />
           </Link>
         </div>
         <div className="col-3"></div>
+      </div>
+      <div className="row container">
+
+        <div className="container col-3 text-center">
+          <NavLink
+            to='/wines'
+            className="tags"
+            activeStyle={{ color: 'red', fontWeight: 'bold'}}>
+            Wines
+          </NavLink>
+        </div>
+        <div className="container col-3 text-center">
+          <NavLink
+            to='/spirits'
+            className="tags"
+            activeStyle={{ color: 'red', fontWeight: 'bold'}}>
+            Spirits
+          </NavLink>
+        </div>
+        <div className="container col-3 text-center">
+        <NavLink 
+            to='/beers'
+            className="tags"
+            activeStyle={{ color: 'red', fontWeight: 'bold'}}>
+            Beers
+        </NavLink>
+        </div>
+        <div className="container col-3 text-center">
+        </div>
+
+
+
       </div>
     </nav>
   )
