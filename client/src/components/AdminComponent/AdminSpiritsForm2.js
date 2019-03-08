05eaa83a6574e5544b4efe24162f56a017296a5e
shinyscyther <mesibovj@gmail.com>
2019-03-07 23:39:58 -0500

    got form functioning

--- client/src/components/AdminComponent/AdminSpiritsForm2.js
+++ client/src/components/AdminComponent/AdminSpiritsForm2.js
@@ -4,7 +4,7 @@ function AdminSpiritsForm2(props) {
   return (
     <div className="container-fluid">
       <div className="card col-4 mb-5">
-      <h3>{JSON.stringify(props.chosenItem)}</h3>
+      <h3>_id: {JSON.stringify(props.chosenItem._id)}</h3>
         <form>
 
           <div className="form-group">
@@ -12,10 +12,44 @@ function AdminSpiritsForm2(props) {
             <textarea className="form-control" id="nameUpdate" rows="1" value={props.chosenItem.ProductName}></textarea>
           </div>
 
-          {/* <div className="form-group">
+          <div className="form-group">
             <label htmlFor="VolumeUpdate">Volume:</label>
             <textarea className="form-control" id="VolumeUpdate" rows="1" value={props.chosenItem.Volume}></textarea>
-          </div> */}
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="AbvUpdate">Abv:</label>
+            <textarea className="form-control" id="AbvUpdate" rows="1" value={props.chosenItem.Abv}></textarea>
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="CategoryUpdate">Category:</label>
+            <textarea className="form-control" id="CategoryUpdate" rows="1" value={props.chosenItem.Category}></textarea>
+          </div>
+          
+          <div className="form-group">
+            <label htmlFor="TypeUpdate">Type:</label>
+            <textarea className="form-control" id="TypeUpdate" rows="1" value={props.chosenItem.Type}></textarea>
+          </div>
+
+          {/* go back and change the props.children.Type below to reflect the subtype once the database is updated to have "SubType" instead of "Sub-Type" */}
+
+          <div className="form-group">
+            <label htmlFor="SubTypeUpdate">SubType:</label>
+            <textarea className="form-control" id="SubTypeUpdate" rows="1" value={props.chosenItem.Type}></textarea>
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="CountryUpdate">Country:</label>
+            <textarea className="form-control" id="CountryUpdate" rows="1" value={props.chosenItem.Country}></textarea>
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="DescriptionUpdate">Description:</label>
+            <textarea className="form-control" id="DescriptionUpdate" rows="5" value={props.chosenItem.Description}></textarea>
+          </div>
+
+
 
         </form>
       </div>
