87f73d0ddee703a24f3b1fcc423f7835e0c3d15f
Priyesh <91priyesh@gmail.com>
2019-03-07 21:04:31 -0500

    changed proper names in seedDB.js

--- seeds.js
+++ seeds.js
@@ -1,46 +1,42 @@
 const random = [
   {
-  "MonthTitle": "March",
-  "MonthSubTitle": ""
-  },
-  {
-    "BrandName": "Harpoon The Craic",
-    "AvaibleSizes": "12oz 6pk Bottles",
-    "BeerStyle": "Irish Red IPA",
-    "Abv": "6.9",
-    "Description": "This hybrid ale combines two classic styles —IPA and Irish Red Ale—to create a unique and flavorful beer. Th­e malt combination commonly used in Irish Red Ales brings a toasted, malty flavor that balances the citrusy and fruity character of the hops.",
+    "FamilyName": "Collier Creek",
+    "Country": "USA",
+    "Size": "750 ml",
+    "Types": "Cabernet Sauvignon, Pinot Noir, Zinfandel Chardonnay",
+    "Description": "Collier Creek is really more of a state of mind, a paddle away from memory lane, and just around the creek bend from the past - A time when farmland ran as far as the eye could see. Nestled between the gold country of the Sierra Foothills and the San Francisco Delta waters lie the heart of the Lodi Appellation, where these fine wines are grown and produced.",
     "ImgUrl": ""
   },
   {
-    "BrandName": "Guinness Draught",
-    "AvaibleSizes": "15oz 4pk cans and 11oz 6pk Bottles",
-    "BeerStyle": "Stout",
-    "Abv": "4.2",
-    "Description": "Ireland- Irish Dry Stout- Pours to a beautiful brown color with a creamy tan head. Smooth roasted malt and coffee flavors are balanced by some hopping, and the mouthfeel is as silky as you could ask for. Only 125 calories per 12 ounce serving.",
+    "FamilyName": "Papi",
+    "Country": "Chile",
+    "Size": "750ml or 1.5 L",
+    "Types": "Cabernet Sauvignon, Merlot, Pinot Noir",
+    "Description": "Fine Chilean wines start with the finest Chilean grapes, and that’s why we choose only those from the most renowned grape growing region of Central Valley. Our renowned wines are the result of the perfect balance, character, and complexity due to our famous tradition and knowledge in the art of winemaking.",
     "ImgUrl": ""
   },
   {
-    "BrandName": "Guinness Foreign Extra Stout",
-    "AvaibleSizes": "12oz 4pk Bottles",
-    "BeerStyle": "Stout",
-    "Abv": "7.5",
-    "Description": "Ireland- Foreign / Export Stout- 7.5% ABV. Black color with ruby highlights. Aromas of toffee, toasted malts and dark fruit. The full body delivers a hoppier taste than regular Guinness and a roasted barley bitterness. Dark fruit character. Heavy mouthfeel. Dry and nutty in the finish.",
+    "FamilyName": "Josh",
+    "Country": "USA",
+    "Size": "750ml",
+    "Types": "Cabernet Sauvignon, Melot",
+    "Description": "",
     "ImgUrl": ""
   },
   {
-    "BrandName": "Guinness Extra Stout",
-    "AvaibleSizes": "11oz 6pk Bottles",
-    "BeerStyle": "Stout",
-    "Abv": "5",
-    "Description": "Ireland- Irish Dry Stout- This beer icon is pitch black in color with roasted malt aromas. It has a rich, creamy mouth feel without being too heavy. The hops provide a nice balance to the malts, allowing a slight sweetness to show on the finish.",
+    "FamilyName": "Zensa",
+    "Country": "Italy",
+    "Size": "750ml",
+    "Types": "Pinot Grigio, Primitivo, Sparkling wine",
+    "Description": "",
     "ImgUrl": ""
   },
   {
-    "BrandName": "Smithwick's Irish Ale",
-    "AvaibleSizes": "11oz 6pk Bottles",
-    "BeerStyle": "Irish Red Ale",
-    "Abv": "4.5",
-    "Description": "Ireland- Irish Red Ale- Smithwick's Irish Ale is a copper colored ale with a thick, pillowy head. The aroma is sweet with malty caramel notes. It has a very clean taste with great hop flavors.",
+    "FamilyName": "Veuve du Vernay Brut",
+    "Country": "France",
+    "Size": "750ml",
+    "Types": "Sparkling Wine",
+    "Description": "",
     "ImgUrl": ""
-  },
+  }
 ]
