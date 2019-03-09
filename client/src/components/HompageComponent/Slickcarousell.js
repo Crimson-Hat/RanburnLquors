2db35ec8542bd74cb4344a2eb9ac6e1abc068a3b
Priyesh <91priyesh@gmail.com>
2019-03-09 12:49:37 -0500

    saving caruosell work

--- client/src/components/HompageComponent/Slickcarousell.js
+++ client/src/components/HompageComponent/Slickcarousell.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import "./style.css";
 
 // import "./slick/slick.css";
 // import "./slick/slick-theme.css";
@@ -25,27 +26,29 @@ export default class SimpleSlider extends Component {
       pauseOnHover: true,
       swipeToSlide: true,
       touchMove: true,
+      adaptiveHeight: false,
+      fade: true,
 
-     };
+    };
     return (
-      <div className="m-auto h-30">
+      <div className="m-auto carouseloutterdiv">
         <Slider {...settings}>
           <div >
-            <img className="d-block text-center w-100" src={One} alt="First slide" />
+            <img className="imagefix text-center w-100" src={One} alt="First slide" />
           </div>
           <div>
-            <img className="d-block w-100" src={Two} alt="Second slide" />
+            <img className="imagefix  w-100" src={Two} alt="Second slide" />
           </div>
           <div>
-            <img className="d-block w-100" src={Three} alt="Third slide" />
+            <img className="imagefix  w-100" src={Three} alt="Third slide" />
           </div>
           <div>
-            <img className="d-block w-100" src={Four} alt="Fourth slide" />
+            <img className="imagefix  w-100" src={Four} alt="Fourth slide" />
           </div>
           <div>
-            <img className="d-block w-100" src={Five} alt="Fifth slide" />
+            <img className="imagefix  w-100" src={Five} alt="Fifth slide" />
           </div>
-          </Slider>
+        </Slider>
       </div>
     );
   }
