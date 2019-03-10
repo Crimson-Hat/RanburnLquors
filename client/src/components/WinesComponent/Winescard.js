36063325ae3b5528fb1ac52cc3660004e1ba2527
Priyesh <91priyesh@gmail.com>
2019-03-10 14:00:18 -0400

    saving card progress on wines page

--- client/src/components/WinesComponent/Winescard.js
+++ client/src/components/WinesComponent/Winescard.js
@@ -19,16 +19,34 @@ class Winescard extends React.Component {
         style={{ width: 'auto', height: 'auto' }} /// these are optional style, it is not necessary
       >
 
-        <FrontSide className="col-12" style={{ backgroundColor: '#41669d' }}>
-          <div>
+        <FrontSide className="p-0 m-3 col-12">
+          <div className="container-fluid p-0">
             <div className="card mx-auto">
-              <div>
-                <img className="card-img-top img-fluid" src={this.props.ImgUrl} alt={this.props.FamilyName} />
-              </div>
-              <div className="card-body">
-                <h4 className="card-title text-center">{this.props.FamilyName}
-                </h4>
-                <div className="row">
+              <div className="card-body p-0 m-0">
+                <div className="cardtitle text-center">{this.props.FamilyName}
+                </div>
+                <div className="container-fluid row m-0 p-0">
+                  <div className="col-6">
+
+
+                  </div>
+
+                  <div className="col-6 p-0 m-0">
+                    <div className="card-img-top text-center">                      
+                        <img className="imgprop" src={this.props.ImgUrl} alt={this.props.FamilyName} />                      
+                    </div>
+
+                  </div>
+
+
+
+
+
+
+                </div>
+
+
+                <div className="container-fluid row m-0 p-0">
                   <div className="col-6 card-font-property pr-0">
                     {this.props.Types}
                     <br />
@@ -42,14 +60,14 @@ class Winescard extends React.Component {
           </div>
         </FrontSide>
 
-        <BackSide style={{ backgroundColor: '#175852' }}>
+        <BackSide className="p-0 col-12">
           <div>
-            <div>
-              <div className="card mx-auto">
-                <div className="card-body">
-                  <h4 className="card-title text-center">{this.props.FamilyName}
-                  </h4>
-                  <div className="row">
+            <div className="card mx-auto">
+              <div className="card-body m-0 p-0">
+                <div className="cardtitle text-center">{this.props.FamilyName}
+                </div>
+                <div className="container-fluid row m-0 p-0">
+                  <div className="container-fluid text-left backcarddescription">
                     {this.props.Description}
                   </div>
                 </div>
