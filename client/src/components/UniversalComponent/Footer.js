002a011690146cd9eba22f794a9b14dc5214f21b
Priyesh <91priyesh@gmail.com>
2019-03-09 22:48:34 -0500

    completed disclaimber page with proper link working in footer

--- client/src/components/UniversalComponent/Footer.js
+++ client/src/components/UniversalComponent/Footer.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 import "./style.css";
 
 function Footer() {
@@ -6,7 +8,20 @@ function Footer() {
   return (
     <React.Fragment>
       <div className="footer">
-      footer
+        <Link to="/disclaimer">
+          <div className="container disclaimerlink">
+            DISCLAIMER
+          </div>
+        </Link>
+
+        <Link to="/designers">
+          <div className="container designerlink">
+            Click Here to contact site designers
+          </div>
+        </Link>
+
+
+
       </div>
     </React.Fragment>
   )
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './index.css';
 import Home from './components/Home';
 import Admin from './components/Admin';
