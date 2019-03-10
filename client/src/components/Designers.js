0e9c0dd9e58f1d1de4594431dcc9eb3ff7597767
shinyscyther <mesibovj@gmail.com>
2019-03-10 15:32:06 -0400

    saving work on designer page. Added links and contact info

--- client/src/components/Designers.js
+++ client/src/components/Designers.js
@@ -7,15 +7,55 @@ class Disclaimer extends React.Component {
     return (
       <React.Fragment>
 
-        <div className="container-fluid designerdiv">
-          <div className="container-fluid row">
+        <div className="container-fluid designerdiv pb-3">
+          <div className="container-fluid row mb-3">
             <div className="container-fluid text-center designertitle">
-              Contact Designers
+              Contact Site Designers
             </div>
           </div>
 
           <div className="container-fluid row text-justify designerbody">
-           Priyesh Patel/Jordan Mesibov
+
+            <div className="col-12 col-sm-4 contact-p pl-5">
+              <h4 className="text-left">Priyesh Patel</h4>
+              <div className="row">
+                E-mail: 91priyesh@gmail.com
+              </div>
+              <div className="row">
+                <a className="designer-link" href="#" target="_blank">LinkedIn</a>
+              </div>
+
+              <div className="row">
+                <a className="designer-link" href="#" target="_blank">Portfolio</a>
+              </div>
+
+              <div className="row">
+                <a className="designer-link" href="#" target="_blank">another link</a>
+              </div>
+            </div>
+
+            <div className="col-12 col-sm-4">
+            
+            </div>
+
+            <div className="col-12 col-sm-4 contact-j pl-5">
+              <h4 className="text-left">Jordan Mesibov</h4>
+              <div className="row">
+                E-mail: mesibovj@gmail.com
+                </div>
+              <div className="row">
+                <a className="designer-link" href="https://www.linkedin.com/in/jordan-mesibov-581538176" target="_blank">LinkedIn</a>
+              </div>
+
+              <div className="row">
+                <a className="designer-link" href="#" target="_blank">Portfolio</a>
+              </div>
+
+              <div className="row">
+                <a className="designer-link" href="#" target="_blank">another link</a>
+              </div>
+            </div>
+
           </div>
 
         </div>
